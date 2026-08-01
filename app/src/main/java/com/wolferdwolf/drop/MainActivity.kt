9253a1516445bf13d24315abf3ae7f882535dfe9
wolferd-wolf <akilawolfs@gmail.com>
2026-08-02 00:26:20 +0530

    Fix Compose Material API opt-in

--- app/src/main/java/com/wolferdwolf/drop/MainActivity.kt
+++ app/src/main/java/com/wolferdwolf/drop/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -31,6 +32,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DropHomeScreen() {
     Scaffold(
