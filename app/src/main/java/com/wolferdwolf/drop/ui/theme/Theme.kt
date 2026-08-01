33777dd4fd1263bd12599151b88833b2e8333546
wolferd-wolf <akilawolfs@gmail.com>
2026-08-02 00:25:18 +0530

    Add Drop Material theme

---
+++ app/src/main/java/com/wolferdwolf/drop/ui/theme/Theme.kt
@@ -0,0 +1,18 @@
+package com.wolferdwolf.drop.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.foundation.isSystemInDarkTheme
+
+private val LightColors = lightColorScheme()
+private val DarkColors = darkColorScheme()
+
+@Composable
+fun DropTheme(content: @Composable () -> Unit) {
+    MaterialTheme(
+        colorScheme = if (isSystemInDarkTheme()) DarkColors else LightColors,
+        content = content
+    )
+}
