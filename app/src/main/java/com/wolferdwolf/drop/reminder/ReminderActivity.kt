81409ac48c6f5e44ba02c85e6f849e7ed42deef0
wolferd-wolf <akilawolfs@gmail.com>
2026-08-09 18:52:54 +0530

    Restore Reminder top app bar import

--- app/src/main/java/com/wolferdwolf/drop/reminder/ReminderActivity.kt
+++ app/src/main/java/com/wolferdwolf/drop/reminder/ReminderActivity.kt
@@ -20,6 +20,7 @@ import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
